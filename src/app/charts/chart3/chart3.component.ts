7df89e7049362bd6c74c73fc8b50dcfec79feae9
d3ataviz <d3ataviz@gmail.com>
2020-12-16 23:26:55 +0100

    d3-transitions v2

--- src/app/charts/chart3/chart3.component.ts
+++ src/app/charts/chart3/chart3.component.ts
@@ -13,8 +13,7 @@ export class Chart3Component implements OnInit, OnChanges {
   dataContainer: any;
   xAxisContainer: any;
   yAxisContainer: any;
-  xAxis: any;
-  yAxis: any;
+  textButton: any;
 
   @Input() data;
 
@@ -23,13 +22,16 @@ export class Chart3Component implements OnInit, OnChanges {
   dimensions: DOMRect;
   innerWidth;
   innerHeight;
-  left = 60; right = 20; bottom = 20; top = 15;
+  left = 60; right = 20; bottom = 20; top = 35;
 
   x = d3.scaleBand().paddingInner(0.2).paddingOuter(0.2);
   y = d3.scaleLinear();
 
-  activeSorting;
-  activeSortingIndex = false;
+  xAxis = d3.axisBottom();
+  yAxis = d3.axisLeft();
+
+  // false = by id, true = by salary
+  sortingState = false;
 
   constructor(element: ElementRef) {
     this.host = d3.select(element.nativeElement);
@@ -38,13 +40,27 @@ export class Chart3Component implements OnInit, OnChanges {
    }
 
   ngOnInit() {
-    this.svg = this.host.select('svg').on('click', () => this.switchSorting());
+    const chart = this;
+    this.svg = this.host.select('svg');
 
     this.setDimensions();
-    this.setElements();
 
-    this.activeSorting = this.alphabetical;
+    this.xAxisContainer = this.svg.append('g').attr('class', 'xAxisContainer')
+    .attr('transform', `translate(${this.left}, ${this.top + this.innerHeight})`);
 
+    this.yAxisContainer = this.svg.append('g').attr('class', 'yAxisContainer')
+    .attr('transform', `translate(${this.left}, ${this.top})`);
+
+    this.dataContainer = this.svg.append('g').attr('class', 'dataContainer')
+    .attr('transform', `translate(${this.left}, ${this.top})`);
+
+    this.textButton = this.svg.append('text')
+    .attr('class', 'text-button')
+    .text('Sort data')
+    .attr('x', 60).attr('y', 20)
+    .on('click', function() {
+      chart.sortData();
+    });
   }
 
   setDimensions() {
@@ -53,72 +69,77 @@ export class Chart3Component implements OnInit, OnChanges {
     this.innerHeight = this.dimensions.height - this.top - this.bottom;
   }
 
-  setElements() {
-    this.xAxisContainer = this.svg.append('g').attr('class', 'xAxisContainer')
-    .attr('transform', `translate(${this.left}, ${this.top + this.innerHeight})`);
-    this.yAxisContainer = this.svg.append('g').attr('class', 'yAxisContainer')
-    .attr('transform', `translate(${this.left}, ${this.top})`);
-    this.dataContainer = this.svg.append('g').attr('class', 'dataContainer')
-    .attr('transform', `translate(${this.left}, ${this.top})`);
-  }
-
   ngOnChanges(changes: SimpleChanges) {
     if(!this.svg) return;
     this.setParams();
+    this.setAxis();
     this.draw();
+  }
+
+  setAxis() {
+    this.xAxis.scale(this.x);
+    this.xAxisContainer.transition().duration(3000).call(this.xAxis);
+
+    this.yAxis.scale(this.y).tickSize(-this.innerWidth);
+    this.yAxisContainer.call(this.yAxis);
+
+    this.yAxisContainer.selectAll('.tick line').style('stroke', '#ddd');
   }
 
   setParams() {
-    const ids = this.data.sort(this.activeSorting).map((d) => d.id);
+    const ids = this.data.map((d) => d.id);
     this.x.domain(ids).range([0, this.innerWidth]);
     const max_salary = 1.3 * Math.max(...this.data.map((item) => item.employee_salary));
     this.y.domain([0, max_salary]).range([this.innerHeight, 0]);
-
-    this.xAxis = d3.axisBottom(this.x);
-    this.yAxis = d3.axisLeft(this.y).tickSize(-this.innerWidth);
-
-    this.xAxisContainer.call(this.xAxis);
-    this.yAxisContainer.call(this.yAxis);
   }
 
   draw() {
+    // bind the data to the rectangles
     const rects = this.dataContainer.selectAll('rect')
-    .data(this.data || [], (d) => d.id);
+    .data(this.data || []);
 
-    // update section
-    rects.attr('width', this.x.bandwidth())
-    .transition()
-    .delay((d, i) => 50 * i)
-    .attr('x', (d) => this.x(d.id))
-    .attr('y', (d) => this.y(d.employee_salary))
-    .attr('height', (d) => this.innerHeight -this.y(d.employee_salary));
-
-    // new rectangles
-    rects.enter().append('rect')
+    rects.transition()
+    .duration(3000)
     .attr('x', (d) => this.x(d.id))
     .attr('width', this.x.bandwidth())
     .attr('y', (d) => this.y(d.employee_salary))
-    .attr('height', (d) => this.innerHeight -this.y(d.employee_salary));
+    .attr('height', (d) => this.innerHeight - this.y(d.employee_salary));
+    // add new rectangles and updating existing ones
+    rects.enter().append('rect')
+    .attr('x', (d) => this.x(d.id))
+    .attr('width', this.x.bandwidth())
+    .attr('y', this.innerHeight)
+    .transition()
+    .duration(3000)
+    .attr('y', (d) => this.y(d.employee_salary))
+    .attr('height', (d) => this.innerHeight - this.y(d.employee_salary));
 
-    // remove not needed
+    // remove rectangles not needed;
     rects.exit().remove();
   }
 
-  alphabetical = (a, b) => +a.id - +b.id;
+  sortData() {
+    // change the sorting state
+    this.sortingState = !this.sortingState;
 
-  ascending = (a, b) => +a.employee_salary - +b.employee_salary;
-
-  switchSorting() {
-    this.activeSortingIndex = !this.activeSortingIndex;
-
-    if (this.activeSortingIndex) {
-      this.activeSorting = this.ascending;
+    // sort the data
+    if (this.sortingState) {
+      // sort by salary
+      this.data.sort(this.sortBYSalary);
     } else {
-      this.activeSorting = this.alphabetical;
+      // sort by id
+      this.data.sort(this.sortById);
     }
-
+    // set params
     this.setParams();
+    // update axis
+    this.setAxis();
+    // draw the chart with new data
     this.draw();
   }
+
+  sortById = (a, b) => +a.id - +b.id;
+
+  sortBYSalary = (a, b) => +b.employee_salary - +a.employee_salary;
 
 }
