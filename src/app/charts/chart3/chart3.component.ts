c5ce5ab7d5d9465526677832f12b55df5237157a
d3ataviz <d3ataviz@gmail.com>
2021-01-15 09:49:00 +0100

    d3-chart3-final

--- src/app/charts/chart3/chart3.component.ts
+++ src/app/charts/chart3/chart3.component.ts
@@ -13,6 +13,7 @@ export class Chart3Component implements OnInit, OnChanges {
   dataContainer: any;
   xAxisContainer: any;
   yAxisContainer: any;
+  textLabel: any;
 
   @Input() data;
 
@@ -23,7 +24,7 @@ export class Chart3Component implements OnInit, OnChanges {
   innerHeight;
   xAxis: any;
   yAxis: any;
-  left = 60; right = 20; bottom = 80; top = 15;
+  left = 60; right = 20; bottom = 80; top = 35;
 
   x = d3.scaleBand().paddingInner(0.2).paddingOuter(0.2);
   y = d3.scaleLinear();
@@ -36,10 +37,6 @@ export class Chart3Component implements OnInit, OnChanges {
     : this.data.sort((a, b) => a.employee_name < b.employee_name ? -1 : 1);
   }
 
-  get ids() {
-    return this.barsData.map((d) => d.id);
-  }
-
   constructor(element: ElementRef) {
     this.host = d3.select(element.nativeElement);
 
@@ -48,25 +45,37 @@ export class Chart3Component implements OnInit, OnChanges {
 
   ngOnInit() {
     this.svg = this.host.select('svg')
-    .on('click', () => this.changeData());
+    .on('click', () => this.dataChanged());
 
     this.setDimensions();
 
     this.setElements();
   }
 
-  changeData() {
+  dataChanged() {
     this.sortedBySalary = !this.sortedBySalary;
     this.updateChart();
   }
 
   setElements() {
-    this.xAxisContainer = this.svg.append('g').attr('class', 'xAxisContainer')
-    .attr('transform', `translate(${this.left}, ${this.top + this.innerHeight})`);
-    this.yAxisContainer = this.svg.append('g').attr('class', 'yAxisContainer')
-    .attr('transform', `translate(${this.left}, ${this.top})`);
-    this.dataContainer = this.svg.append('g').attr('class', 'dataContainer')
-    .attr('transform', `translate(${this.left}, ${this.top})`);
+    this.xAxisContainer = this.svg.append('g')
+      .attr('class', 'xAxisContainer')
+      .attr('transform', `translate(${this.left}, ${this.top + this.innerHeight})`);
+
+    this.yAxisContainer = this.svg.append('g')
+      .attr('class', 'yAxisContainer')
+      .attr('transform', `translate(${this.left}, ${this.top})`);
+
+    this.dataContainer = this.svg.append('g')
+      .attr('class', 'dataContainer')
+      .attr('transform', `translate(${this.left}, ${this.top})`);
+
+    this.textLabel = this.svg.append('g')
+      .attr('transform', `translate(${0.5 * this.dimensions.width}, 20)`)
+      .append('text')
+      .attr('class', 'label')
+      .style('text-anchor', 'middle')
+      .style('font-weight', 'bold');
   }
 
   setDimensions() {
@@ -89,32 +98,44 @@ export class Chart3Component implements OnInit, OnChanges {
   }
 
   setLabels() {
-    this.svg.append('g')
-    .attr('transform', `translate(15, ${0.5 * this.innerHeight + this.top})`)
-    .append('text').attr('class', 'label').text('Employee salary')
-    .attr('transform', `rotate(-90)`)
-    .style('text-anchor', 'middle');
-
-    this.xAxisContainer.selectAll('.tick text').text(this.getEmployeeName)
-    .attr('transform', 'translate(-10,2)rotate(-45)')
-    .style('text-anchor', 'end');
+    this.textLabel.text('Employee salary');
   }
 
   getEmployeeName = (id) => this.data.find((d) => d.id === id).employee_name;
 
   setAxis() {
+
+    const updateXAxis = (xAxisContainer) => {
+      xAxisContainer.call(this.xAxis);
+
+      xAxisContainer
+      .selectAll('.tick text')
+      .attr('transform', 'translate(-10,2)rotate(-45)')
+      .style('text-anchor', 'end')
+      .text(this.getEmployeeName);
+    };
+
     this.xAxis = d3.axisBottom(this.x);
-    this.xAxisContainer.call(this.xAxis);
 
-    this.yAxis = d3.axisLeft(this.y).tickSizeInner(-this.innerWidth).tickFormat(d3.format('$~s'));
-    this.yAxisContainer.call(this.yAxis);
+    this.xAxisContainer
+      .transition()
+      .duration(500)
+      .call(updateXAxis);
 
-    this.yAxisContainer.selectAll('.tick line').style('stroke', '#ddd');
-    // this.yAxisContainer.selectAll('.tick text').style('fill', '#ddd');
+    this.yAxis = d3.axisLeft(this.y)
+      .tickSizeInner(-this.innerWidth)
+      .tickFormat(d3.format('$~s'));
+
+      this.yAxisContainer.call(this.yAxis);
+
+    this.yAxisContainer
+      .selectAll('.tick line')
+      .style('stroke', '#ddd');
   }
 
   setParams() {
-    this.x.domain(this.ids).range([0, this.innerWidth]);
+    const ids = this.barsData.map((d) => d.id);
+    this.x.domain(ids).range([0, this.innerWidth]);
     const max_salary = 1.3 * Math.max(...this.data.map((item) => item.employee_salary));
     this.y.domain([0, max_salary]).range([this.innerHeight, 0]);
   }
@@ -125,6 +146,8 @@ export class Chart3Component implements OnInit, OnChanges {
 
     bars.enter().append('rect')
     .merge(bars)
+    .transition()
+    .duration(500)
     .attr('x', (d) => this.x(d.id))
     .attr('width', this.x.bandwidth())
     .attr('y', (d) => this.y(d.employee_salary))
