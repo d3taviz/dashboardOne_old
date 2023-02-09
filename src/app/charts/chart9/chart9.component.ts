83a85ed1dda2fe85afd3d3677fb5e68969792fb7
d3taviz <d3taviz@gmail.com>
2023-02-09 12:20:32 +0100

    chart 9 added

---
+++ src/app/charts/chart9/chart9.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart9',
+  template: `
+    <p>
+      chart9 works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Chart9Component {
+
+}
