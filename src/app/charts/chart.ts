49f5d9f34c9e6ad9c3844645cbe9256be38b4054
d3taviz <d3taviz@gmail.com>
2023-02-13 18:56:19 +0100

    setting methods p2

--- src/app/charts/chart.ts
+++ src/app/charts/chart.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angular/core";
 import { Selection } from 'd3-selection';
 import * as d3 from 'd3';
-import { Subscription } from "rxjs";
+import { debounceTime, fromEvent, Subscription, map } from "rxjs";
 import { IBaseConfig } from "../interfaces/chart.interfaces";
 import { DimensionsService } from "../services/dimensions.service";
 import { ObjectHelper } from '../helpers/object.helper';
@@ -41,7 +41,7 @@ export abstract class Chart<D, C extends IBaseConfig> implements OnInit, OnDestr
     protected host: Selection<any, any, any, any> = null as any;
     protected svg: Selection<SVGSVGElement, any, any, any> = null as any;
 
-    protected subscriptions: Subscription[] = [];
+    private subscriptions: Subscription[] = [];
 
     chartIsInitialized: boolean = false;
     dataIsInitialized: boolean = false;
@@ -51,13 +51,27 @@ export abstract class Chart<D, C extends IBaseConfig> implements OnInit, OnDestr
     protected abstract _defaultConfig: C;
 
     // methods
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.setSubscriptions();
 
-    ngOnDestroy(): void {
-        
+        this.setSvg();
+        this.setElements();
+        this.chartIsInitialized = true;
+        this.updateChart();
     }
 
-    updateChart(): void {}
+    ngOnDestroy(): void {
+        this.unsubscribeAll();
+    }
+
+    updateChart() {
+        this.setDimensions();
+        this.positionElements();
+        this.setParams();
+        this.setLabels();
+        this.setLegend();
+        this.draw();
+      }
 
     // run once
     setSvg(): void {}
@@ -80,13 +94,30 @@ export abstract class Chart<D, C extends IBaseConfig> implements OnInit, OnDestr
 
 
     // subscriptions
-    subscribe(sub: Subscription): void {}
+    subscribe(sub: Subscription): void {
+        this.subscriptions.push(sub);
+    }
 
-    unsubscribeAll(): void {}
+    unsubscribeAll(): void {
+        this.subscriptions.map((sub: Subscription) => sub.unsubscribe());
+    }
 
-    setSubscriptions(): void {}
+    setSubscriptions(): void {
+        this.setResize();
+    }
 
-    setResize(): void {}
+    setResize(): void {
+        const resize$ = fromEvent(window, 'resize');
+
+        const subs = resize$
+        .pipe(
+            map((event: any) => event),
+            debounceTime(500)
+        )
+        .subscribe(() => this.updateChart());
+
+        this.subscribe(subs);
+    }
 
 
     // set data and config
